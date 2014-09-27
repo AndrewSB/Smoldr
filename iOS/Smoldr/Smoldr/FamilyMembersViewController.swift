07915f4930c52755dad4c89df4c6f26fa8854a1d
Justin Loew <justinl@wolfram.com>
2014-09-27 14:39:41 -0500

    fixed icons, added code somewhere.

---
+++ iOS/Smoldr/Smoldr/FamilyMembersViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FamilyMembersViewController.swift
+//  Smoldr
+//
+//  Created by Justin Loew on 9/27/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import Foundation
+import AddressBook
+
+
